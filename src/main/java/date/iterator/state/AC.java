bb35d639e7545d5aa6b1088af4894b0c89e3c152
aaa <dravulav@hotmail.com>
2020-04-14 19:02:56 +0800

    ACTest

--- src/main/java/date/iterator/state/AC.java
+++ src/main/java/date/iterator/state/AC.java
@@ -15,17 +15,55 @@ public class AC {
     }
 
     public void search(final String input) {
+        CharNode searched = trie.getRoot();
         for (int i = 0; i < input.length(); i++) {
-            search(input.charAt(i));
+            searched = search(input.charAt(i), searched);
+            if (searched == null) {
+                searched = trie.getRoot();
+                continue;
+            }
+            if (searched.getOriginLengths() != null) {
+                for (Integer originLength : searched.getOriginLengths()) {
+                    // todo 找的不全
+                    System.out.println(searched.getValue() + " : " + originLength);
+                    System.out.println(input.substring(i + 1 - originLength, i + 1)); //extends to the character at index {@code endIndex - 1}
+                }
+            }
         }
     }
 
-    public void search(final char input) {
+    /*public void search(final char input) {
         CharNode node = trie.getRoot();
         Map<Character, CharNode> searchingNodes = node.getSubNodes();
         CharNode currentNode = trie.getRoot();
         while (currentNode.getSubNodes() != null && !currentNode.getSubNodes().isEmpty()) {
-            CharNode searched = currentNode.getSubNodes().get(input);
+            if (currentNode.getSubNodes().containsKey(input)) {
+                CharNode searched = currentNode.getSubNodes().get(input);
+                currentNode = searched;
+            } else {
+                if (currentNode.getLargestStrSub() == null) {
+                    currentNode = trie.getRoot();
+                } else {
+                    currentNode = currentNode.getLargestStrSub();
+                }
+            }
         }
+    }*/
+
+    public CharNode search(final char input, final CharNode currentNode) {
+        CharNode result = null;
+        if (currentNode.getSubNodes() != null && !currentNode.getSubNodes().isEmpty()) {
+            if (currentNode.getSubNodes().containsKey(input)) {
+                CharNode searched = currentNode.getSubNodes().get(input);
+                result = searched;
+            } else {
+                if (currentNode.getLargestStrSub() == null) {
+                    result = trie.getRoot();
+                } else {
+                    result = currentNode.getLargestStrSub();
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/date/iterator/state/ACTest.java
+++ src/test/java/date/iterator/state/ACTest.java
@@ -1,4 +1,13 @@
 package date.iterator.state;
 
+import org.junit.Test;
+
 public class ACTest {
+    @Test
+    public void testSearching() {
+        String[] ss = new String[] {"abcde", "ade", "abc", "dee", "eade"};
+        AC ac = new AC();
+        ac.init(ss);
+        ac.search("abcdeeadeeeabceade");
+    }
 }
