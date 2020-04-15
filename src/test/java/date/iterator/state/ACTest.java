cb16a03116d3e2eb2c34af2fa80da5d89f281b52
aaa <dravulav@hotmail.com>
2020-04-15 16:46:40 +0800

    ac test

--- src/test/java/date/iterator/state/ACTest.java
+++ src/test/java/date/iterator/state/ACTest.java
@@ -1,9 +1,11 @@
 package date.iterator.state;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ACTest {
     @Test
@@ -24,21 +26,44 @@ public class ACTest {
     // abcde 0
     // abc 0,11
     // dee 3,7,16
-    // eade 14
+    // eade 5,14
     @Test
     public void testSearchCharacter() {
+        Map<String, String> expected = new HashMap<>();
+        expected.put("ade", "6,15");
+        expected.put("abcde", "0");
+        expected.put("abc", "0,11");
+        expected.put("dee", "3,7,16");
+        expected.put("eade", "5,14");
+
         String[] ss = new String[] {"abcde", "ade", "abc", "dee", "eade"};
         AC ac = new AC();
         ac.init(ss);
         String input = "abcdeeadeeeabceadee";
+        Map<String, String> actual = new HashMap<>();
         for (int i = 0; i < input.length(); i++) {
+            if (i == 14) {
+                System.out.println();
+            }
             Collection<CharNode> searched = ac.search(input.charAt(i));
             for (CharNode each : searched) {
                 for (Integer originLength : each.getOriginLengths()) {
                     int beginIndex = i + 1 - originLength;
-                    System.out.println(input.substring(beginIndex, i + 1) + " , at index:" + beginIndex);
+                    String achieved = input.substring(beginIndex, i + 1);
+                    if (actual.containsKey(achieved)) {
+                        String value = actual.get(achieved);
+                        value += "," + beginIndex;
+                        actual.put(achieved, value);
+                    } else {
+                        actual.put(achieved, String.valueOf(beginIndex));
+                    }
                 }
             }
+        }
+        for (String key : actual.keySet()) {
+            String expectedValue = expected.get(key);
+            String actualValue = actual.get(key);
+            Assert.assertEquals(expectedValue, actualValue);
         }
     }
 }
